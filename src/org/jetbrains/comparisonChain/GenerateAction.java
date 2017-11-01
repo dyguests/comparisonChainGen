d2d9fcda09cc09826529edfbf4a246c3dbf7cd8c
fanhl <fanhonglin@klicen.com>
2017-11-01 14:05:27 +0800

    Init Commit.

---
+++ src/org/jetbrains/comparisonChain/GenerateAction.java
@@ -0,0 +1,12 @@
+package org.jetbrains.comparisonChain;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class GenerateAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
